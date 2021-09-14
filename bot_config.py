c6b94b19ba8e05f14e9c9cf4e95adcc5e2084f75
Beimo <66893970+Ganyu2007@users.noreply.github.com>
2021-09-14 19:33:15 +0800

    Delete bot_config.py

--- bot_config.py
+++
@@ -1,7 +0,0 @@
-from nonebot.default_config import *
-
-SUPERUSERS = {2023491392}
-COMMAND_START = {'@', '/', '!', '／', '！',''}
-NICKNAME = {'王小美', 'Ganyu', '甘雨', ''}
-HOST = '127.0.0.1'
-PORT = 8080
